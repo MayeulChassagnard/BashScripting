fe87291bb575e65b0fcd792e73ca36bcae1d5ae9
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2017-02-03 15:37:41 -0500

    ENH: Update PATH immediately
    In addition of the update of ~/.profile which allow to keep the configuration
    even after restarting the computer, we need the command to be usable as soon
    as possible

--- install_cmd.sh
+++ install_cmd.sh
@@ -13,8 +13,11 @@ done
 # Remove tmp file
 rm $script_dir/tmp_cmd_name_file.txt
 
-# Update PATH in ~/.profile
+# Update PATH in ~/.profile (even after restarting the computer)
 echo '
 # set PATH to include personal cmd from' $script_dir'/cmd' >> $HOME/.profile
 echo 'export PATH=$PATH":'$script_dir'/cmd"
 ' >> $HOME/.profile
+
+# Update the PATH right now
+export PATH=$PATH":$script_dir/cmd"
